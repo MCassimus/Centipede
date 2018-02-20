dbce21bb81ada18b1acfa2f7f7bfdd90405cea93
Gooland <ryanupham10@gmail.com>
2018-02-20 14:01:58 -0600

    Bullets, mushrooms, scorpions oh my

--- Centipede/Centipede/Bullet.cpp
+++ Centipede/Centipede/Bullet.cpp
@@ -12,12 +12,16 @@ Bullet::Bullet(sf::RenderWindow * renderWindow) : GameObject (renderWindow)
 }
 
 
+static int delay = 0;
 void Bullet::update()  //changes position by velocity
 {
 	currentPosition.x += velocity.x;
 	currentPosition.y += velocity.y;
 	if (currentPosition.x <= 0 || currentPosition.y <= 0)
 		health = 0;
+
+	if (++delay == 5)
+		delay = 0;
 
 	render();
 	return;
--- Centipede/Centipede/Scorpion.cpp
+++ Centipede/Centipede/Scorpion.cpp
@@ -1,10 +1,12 @@
 #include "stdafx.h"
 #include "Scorpion.h"
+#include "GameObject.h"
 
 
 Scorpion::Scorpion(sf::RenderWindow * renderWindow) : GameObject(renderWindow)
 {
 	pointValue = 1000;
+	setVelocity(sf::Vector2i())
 }
 
 
