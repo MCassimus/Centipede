6cb58e024faaab4e3b960052348b025882e30272
Michael Cassimus <MCassimus@users.noreply.github.com>
2018-03-02 14:20:35 -0600

    begin working on flea some more

--- Centipede/Centipede/Flea.cpp
+++ Centipede/Centipede/Flea.cpp
@@ -13,8 +13,8 @@ Flea::Flea(sf::RenderWindow * renderWindow) : GameObject(renderWindow)
 {
 	setTexture("../Sprites/flea.png");
 	object.setScale(sf::Vector2f(.5,.5));
-	object.setColor(sf::Color::White);
-	velocity = sf::Vector2i(0, -1);
+	object.setPosition(sf::Vector2f(window->getSize().x / 2, window->getSize().y / 2));
+	velocity = sf::Vector2i(0, 1);
 	points = 200;
 }
 
@@ -29,10 +29,12 @@ void Flea::update()
 {
 	if (frame++ == delay)//if time to update
 	{
+		object.setPosition(object.getPosition().x, object.getPosition().y + velocity.y);
+		object.setPosition(static_cast<sf::Vector2f>(getNearestCellPos(static_cast<sf::Vector2i>(object.getPosition()))));
 		//apply velocity
-		object.setPosition(currentPosition.x * (window->getSize().x / 29), currentPosition.y * (window->getSize().y / 29));
+		/*object.setPosition(currentPosition.x * (window->getSize().x / 29), currentPosition.y * (window->getSize().y / 29));
 		currentPosition = sf::Vector2u(currentPosition.x, currentPosition.y - velocity.y);
-		
+		*/
 		//reset frame counter
 		frame = 0;
 	}
