7e5d9f58e334200a669c599f1a907029bc4014fa
Gooland <ryanupham10@gmail.com>
2018-03-14 13:55:15 -0500

    spider man spider mand duh duh duh duh duh duh duh

--- Centipede/Centipede/Spider.cpp
+++ Centipede/Centipede/Spider.cpp
@@ -2,27 +2,20 @@
 #include "Spider.h"
 
 
-Spider::Spider(sf::RenderWindow * renderWindow, int x, int y) : GameObject(renderWindow, x,  y)
+Spider::Spider(sf::RenderWindow * renderWindow, int x, int y, Player& p) : GameObject(renderWindow, x,  y)
 {
+	currentPosition.x = x;
+	currentPosition.y = y;
 	setTexture("spider.png");
 	pointValue = 0;
+	player = &p;
 }
 
 
 void Spider::update()
 {
-	setPixels();
-	//while spider exists
-	{
-		// if collision with mushroom
-		{
-			//remove mushroom
-		}
-	}
-	//else
-	{
-		//spawn on screen with 1/400 chance
-	}
+	
+	
 
 	return;
 }
@@ -31,11 +24,11 @@ void Spider::update()
 void Spider::setPointValue()
 {
 	if (distanceFromPlayer > 4) //if more than 4 levels away
-		pointValue = 300;
+		pointValue = 900;
 	else if (distanceFromPlayer < 4 || distanceFromPlayer == 4) //no more than 4 levels
 		pointValue = 600;
 	else if (distanceFromPlayer == 1) //no more than 1 level away 
-		pointValue = 900;
+		pointValue = 300;
 
 	return;
 }
@@ -44,6 +37,40 @@ void Spider::setPointValue()
 bool Spider::takeHealth()
 {
 	return true;
+}
+
+
+void Spider::findDistancePlayer()
+{
+	if(((player->getPosition().x <= currentPosition.x + 1)&&(player->getPosition().x >= currentPosition.x -1)))
+		if (((player->getPosition().y <= currentPosition.y + 1) && (player->getPosition().y >= currentPosition.y - 1)))
+		{
+			pointValue = 300;
+			return;
+		}
+	
+	else if (((player->getPosition().x <= currentPosition.x + 4) && (player->getPosition().x >= currentPosition.x - 4)))
+		if (((player->getPosition().y <= currentPosition.y + 4) && (player->getPosition().y >= currentPosition.y - 4)))
+		{
+			pointValue = 600;
+			return;
+		}
+		
+	else
+	{
+		pointValue = 900;
+		return;
+	}
+}
+
+
+void Spider::colideWith(GameObject* other)
+{
+	if (dynamic_cast<Bullet*>(other) != nullptr)
+		health = 0;
+	else if (dynamic_cast<Player*>(other) != nullptr)
+		health = 0;
+	return;
 }
 
 
