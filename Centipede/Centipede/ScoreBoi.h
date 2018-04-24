865a07b6dc9f8c36a63d032b3c935ba240f385c7
Gooland <ryanupham10@gmail.com>
2018-04-24 13:58:34 -0500

    ScoreBoi

---
+++ Centipede/Centipede/ScoreBoi.h
@@ -0,0 +1,22 @@
+#pragma once
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+class ScoreBoi
+{
+public:
+	ScoreBoi();
+	std::string getScoreX(int);// recieves an int that is the place for a score and returns that score
+	void addScore(std::string);
+	void addScore(int, std::string);
+	~ScoreBoi();
+private:
+	std::ifstream score_in_file;
+	std::ofstream score_out_file;
+	std::string inStuff;
+	int cutScore(std::string);
+};
+
